986d42a7f5175fb2bf4463078c95c979df1ae9fe
ooooo <zhangtongyuan.cpp@gmail.com>
2016-11-09 09:51:23 +0800

    main script file
    main script file

---
+++ BDWM bbs theme.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         BDWM bbs theme
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  A simple script for BDWM bbs website theme.
+// @author       DoubleZ
+// @match        https://bbs.pku.edu.cn/*
+// @match        https://www.bdwm.net/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    var src = ["/v2/uploads/logo_tgfeWm.gif", "/v2/uploads/logo_qFiW2O.gif", "/v2/images/home/logo-rio2016.gif", "/v2/uploads/logo_qGZe5Y.png", "/v2/uploads/logo_zrw8cY.gif"];
+    $("#logo a img").attr("src", src[num]);
+})();
