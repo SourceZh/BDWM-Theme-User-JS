c4812b242bbca6e4e502be380c4b615ee66251ae
ooooo <zhangtongyuan.cpp@gmail.com>
2016-11-09 09:51:49 +0800

    small modify

--- BDWM bbs theme.user.js
+++ BDWM bbs theme.user.js
@@ -14,5 +14,6 @@
 
     // Your code here...
     var src = ["/v2/uploads/logo_tgfeWm.gif", "/v2/uploads/logo_qFiW2O.gif", "/v2/images/home/logo-rio2016.gif", "/v2/uploads/logo_qGZe5Y.png", "/v2/uploads/logo_zrw8cY.gif"];
+    var num = Math.floor(Math.random()*5);
     $("#logo a img").attr("src", src[num]);
 })();
