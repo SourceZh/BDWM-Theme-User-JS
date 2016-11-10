407a5196c10c98d96ce04b87a1ed890b1713f47f
DoubleZ <zhangtongyuan.cpp@gmail.com>
2016-11-10 18:15:25 +0800

    添加注释
    添加了每一个链接对应图片的说明

--- BDWM bbs theme.user.js
+++ BDWM bbs theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BDWM bbs theme
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  A simple script for BDWM bbs website theme.
 // @author       DoubleZ
 // @match        https://bbs.pku.edu.cn/*
@@ -13,7 +13,13 @@
     'use strict';
 
     // Your code here...
-    var src = ["/v2/uploads/logo_qmOATc.gif","/v2/uploads/logo_tgfeWm.gif", "/v2/uploads/logo_qFiW2O.gif", "/v2/images/home/logo-rio2016.gif", "/v2/uploads/logo_qGZe5Y.png", "/v2/uploads/logo_zrw8cY.gif"];
+    var src = ["/v2/uploads/logo_qmOATc.gif",//11.09 拒绝狗粮，从我做起
+    			"/v2/uploads/logo_tgfeWm.gif", // 摇晃的狐狸
+    			"/v2/uploads/logo_qFiW2O.gif", // 翻滚吧，月饼君
+    			"/v2/images/home/logo-rio2016.gif", // 为中国代表团加油
+    			"/v2/uploads/logo_qGZe5Y.png", // 欢迎新同学
+    			"/v2/uploads/logo_zrw8cY.gif" // 祖国生快(67)
+    		];
     var num = Math.floor(Math.random()*6);
     $("#logo a img").attr("src", src[num]);
 })();
