4177c810e4ecfd71c80545305eced5d9104a3b23
SourceZh <tongyuanzh@gmail.com>
2016-11-20 13:08:24 +0800

    userjs
    11.20 主题更新-雪人

--- BDWM bbs theme.user.js
+++ BDWM bbs theme.user.js
@@ -13,13 +13,14 @@
     'use strict';
 
     // Your code here...
-    var src = ["/v2/uploads/logo_qmOATc.gif",//11.09 拒绝狗粮，从我做起
+    var src = [ "/v2/uploads/logo_Tn8pTJ.gif", //11.20 打嗝的雪人
+                "/v2/uploads/logo_qmOATc.gif",//11.09 拒绝狗粮，从我做起
     			"/v2/uploads/logo_tgfeWm.gif", // 摇晃的狐狸
     			"/v2/uploads/logo_qFiW2O.gif", // 翻滚吧，月饼君
     			"/v2/images/home/logo-rio2016.gif", // 为中国代表团加油
     			"/v2/uploads/logo_qGZe5Y.png", // 欢迎新同学
     			"/v2/uploads/logo_zrw8cY.gif" // 祖国生快(67)
     		];
-    var num = Math.floor(Math.random()*6);
+    var num = Math.floor(Math.random()*7);
     $("#logo a img").attr("src", src[num]);
 })();
