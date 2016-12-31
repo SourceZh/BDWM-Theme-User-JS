8d8380f071e6fa00cd6f4c4395a897215e656f24
DoubleZ <zhangtongyuan.cpp@gmail.com>
2016-12-31 12:06:40 +0800

    主题更新
    增加新年主题

--- BDWM bbs theme.user.js
+++ BDWM bbs theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BDWM bbs theme
 // @namespace    https://github.com/SourceZh/BDWM-Theme-User-JS/
-// @version      0.5
+// @version      0.51
 // @description  A simple script for BDWM bbs website theme.
 // @author       DoubleZ
 // @match        https://bbs.pku.edu.cn/*
@@ -13,14 +13,16 @@
     'use strict';
 
     // Your code here...
-    var src = [ "/v2/uploads/logo_Tn8pTJ.gif", //11.20 打嗝的雪人
-                "/v2/uploads/logo_qmOATc.gif",//11.09 拒绝狗粮，从我做起
-    			"/v2/uploads/logo_tgfeWm.gif", // 摇晃的狐狸
-    			"/v2/uploads/logo_qFiW2O.gif", // 翻滚吧，月饼君
-    			"/v2/images/home/logo-rio2016.gif", // 为中国代表团加油
-    			"/v2/uploads/logo_qGZe5Y.png", // 欢迎新同学
-    			"/v2/uploads/logo_zrw8cY.gif" // 祖国生快(67)
-    		];
+    var src = [ 
+        "/v2/uploads/logo_89AF6t.gif", //2016.12.31 2017新年快乐
+        "/v2/uploads/logo_Tn8pTJ.gif", //2016.11.20 打嗝的雪人
+        "/v2/uploads/logo_qmOATc.gif", //2016.11.09 拒绝狗粮，从我做起
+    	"/v2/uploads/logo_tgfeWm.gif", // 摇晃的狐狸
+		"/v2/uploads/logo_qFiW2O.gif", // 翻滚吧，月饼君
+		"/v2/images/home/logo-rio2016.gif", // 为中国代表团加油
+		"/v2/uploads/logo_qGZe5Y.png", // 欢迎新同学
+		"/v2/uploads/logo_zrw8cY.gif"  // 祖国生快(67)
+	];
     var num = Math.floor(Math.random()*src.length);
     $("#logo a img").attr("src", src[num]);
 })();
