349da7f56030b8a97654fed509fceaeb7920e1d6
DoubleZ <zhangtongyuan.cpp@gmail.com>
2017-02-12 11:56:26 +0800

    new theme

--- BDWM bbs theme.user.js
+++ BDWM bbs theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BDWM bbs theme
 // @namespace    https://github.com/SourceZh/BDWM-Theme-User-JS/
-// @version      0.6
+// @version      0.61
 // @description  A simple script for BDWM bbs website theme.
 // @author       DoubleZ
 // @match        https://bbs.pku.edu.cn/*
@@ -13,9 +13,10 @@
     'use strict';
 
     // Your code here...
-    var src = [ "/v2/uploads/logo_s9yX2c.gif", // 2017.01.20 鞭炮声里迎新年
-                "/v2/uploads/logo_Tn8pTJ.gif", // 2016.11.20 打嗝的雪人
+    var src = [ "/v2/uploads/logo_OK8m2o.gif", // 2017.02.12 15的月亮16圆
                 "/v2/uploads/logo_tgfeWm.gif", // 摇晃的狐狸
+                "/v2/uploads/logo_s9yX2c.gif", // 2017.01.20 鞭炮声里迎新年
+                "/v2/uploads/logo_Tn8pTJ.gif", // 2016.11.20 打嗝的雪人
                 "/v2/uploads/logo_89AF6t.gif", // 2016.31 2017新年快乐
                 "/v2/uploads/logo_qmOATc.gif", // 2016.11.09 拒绝狗粮，从我做起
     			"/v2/uploads/logo_qFiW2O.gif", // 翻滚吧，月饼君
@@ -26,7 +27,7 @@
     if ($("#logo a img").attr("src")!=src[0] && $("#logo a img").attr("src")!="/v2/uploads/logo_ibfb2M.png"){
         alert("There is a new theme, please update the script file!");
     }else{
-        var num = Math.floor(Math.random()*3);
+        var num = Math.floor(Math.random()*2);
         $("#logo a img").attr("src", src[num]);
     }
 })();
