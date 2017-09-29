aad0c3a2d6d1868ad6b7f8cf1b44d3e73785fefd
DoubleZ <zhangtongyuan.cpp@gmail.com>
2017-09-29 11:23:08 +0800

    add new theme

--- BDWM bbs theme.user.js
+++ BDWM bbs theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BDWM bbs theme
 // @namespace    https://github.com/SourceZh/BDWM-Theme-User-JS/
-// @version      0.64
+// @version      0.65
 // @description  A simple script for BDWM bbs website theme.
 // @author       DoubleZ
 // @match        https://bbs.pku.edu.cn/*
@@ -14,13 +14,14 @@
 
     // Your code here...
     var src = [ 
-    	"/v2/uploads/logo_A61R4E.gif", //2017.08.15 懒惰的鸡腿
-		"/v2/uploads/logo_TOIvA1.gif", //2017.05.05 融化的冰淇淋
-        "/v2/uploads/logo_OK8m2o.gif", //2017.02.12 15的月亮16圆
-        "/v2/uploads/logo_s9yX2c.gif", //2017.01.20 鞭炮声里迎新年
-        "/v2/uploads/logo_89AF6t.gif", //2016.12.31 2017新年快乐
-        "/v2/uploads/logo_Tn8pTJ.gif", //2016.11.20 打嗝的雪人
-        "/v2/uploads/logo_qmOATc.gif", //2016.11.09 拒绝狗粮，从我做起
+        "/v2/uploads/logo_pII925.gif", // 2017.09.29 欢度国庆&中秋
+    	"/v2/uploads/logo_A61R4E.gif", // 2017.08.15 懒惰的鸡腿
+		"/v2/uploads/logo_TOIvA1.gif", // 2017.05.05 融化的冰淇淋
+        "/v2/uploads/logo_OK8m2o.gif", // 2017.02.12 15的月亮16圆
+        "/v2/uploads/logo_s9yX2c.gif", // 2017.01.20 鞭炮声里迎新年
+        "/v2/uploads/logo_89AF6t.gif", // 2016.12.31 2017新年快乐
+        "/v2/uploads/logo_Tn8pTJ.gif", // 2016.11.20 打嗝的雪人
+        "/v2/uploads/logo_qmOATc.gif", // 2016.11.09 拒绝狗粮，从我做起
     	"/v2/uploads/logo_tgfeWm.gif", // 摇晃的狐狸
 		"/v2/uploads/logo_qFiW2O.gif", // 翻滚吧，月饼君
 		"/v2/images/home/logo-rio2016.gif", // 为中国代表团加油
