f600a5b0935eb2320202fea52f5cb910a08d429d
DoubleZ <qwqw1452@sina.com>
2024-06-07 10:47:47 +0800

    Update BDWM bbs theme.user.js

--- BDWM bbs theme.user.js
+++ BDWM bbs theme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BDWM bbs theme
 // @namespace    https://github.com/SourceZh/BDWM-Theme-User-JS/
-// @version      0.993
+// @version      0.994
 // @description  A simple script for BDWM bbs website theme.
 // @author       DoubleZ
 // @match        https://bbs.pku.edu.cn/*
